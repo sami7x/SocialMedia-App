546719dc840359e49b1f1bdf988d94aaf8033815
vajrasami <vajrsami@gmail.com>
2024-02-01 20:49:56 +0545

    Update and Delete feature added

--- controller/blog-controller.js
+++ controller/blog-controller.js
@@ -1,9 +1,9 @@
 import Blog from "../model/Blog.js";
 
-export const getAllBlogs = async(req,res,next)=> {
-    /* Code use for Testing */
+export const getAllBlogs = async (req, res, next) => {
+  /* Code use for Testing */
 
-    /** let blogs;
+  /** let blogs;
     try{
         blogs = await Blog.find();
     }
@@ -18,60 +18,91 @@ export const getAllBlogs = async(req,res,next)=> {
 
     return res.status(200).json({blogs}); **/
 
-    /*Code used for Production */    
-    try{
-        const blogs = await Blog.find();
-        if(!blogs || blogs.length == 0)
-        {
-            return res.status(404).json({message: "No Blogs found"});
-        }
-        return res.status(200).json({blogs});
+  /*Code used for Production */
 
+  try {
+    const blogs = await Blog.find();
+    if (!blogs || blogs.length == 0) {
+      return res.status(404).json({ message: "No Blogs found" });
     }
-    catch(error){
-        console.log(error);
-        return res.status(500).json({message: "Internal Server Error"});
-    }
-
+    return res.status(200).json({ blogs });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 
-export const addBlog = async(req,res, next)=> {
-    const {title, description, image, user} = req.body;
-    
-    //Basic Validation
-    if(!title || !description || !image || !user)
-    {
-        return res.status(402). json({message: " All fields are madatory"});
-    }
-    //Validation for Description Length
-    if(description.length < 10 || description.length >30)
-    {
-        return res.status(402).json({message: "Description must be between 10 and 30 characters."});
-    }
-    //Query for savig Data in DB
-    const blog = new Blog({
-        title,
-        description, 
-        image,
-        user,
+/**
+ * Add Blog
+ */
+export const addBlog = async (req, res, next) => {
+  const { title, description, image, user } = req.body;
+
+  //Basic Validation
+  if (!title || !description || !image || !user) {
+    return res.status(402).json({ message: " All fields are madatory" });
+  }
+  //Validation for Description Length
+  if (description.length < 10 || description.length > 30) {
+    return res
+      .status(402)
+      .json({ message: "Description must be between 10 and 30 characters." });
+  }
+  //Query for savig Data in DB
+  const blog = new Blog({
+    title,
+    description,
+    image,
+    user,
+  });
+
+  try {
+    await blog.save();
+    return res.status(200).json({ blog });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal Server error" });
+  }
+};
+
+/*
+ * For Editing Blog
+ */
+
+export const updateBlog = async (req, res, next) => {
+  try {
+    const blogId = req.params.blogId;
+    const updateBlog = await Blog.findByIdAndUpdate(blogId, req.body, {
+      new: true,
     });
-    
-    try{
-        await blog.save();  
-        return res.status(200).json({blog});      
+
+    if (!updateBlog) {
+      return res.status(404).json({ message: "No Blogs found." });
     }
-    catch(error)
-    {
-        console.log(error);
-        return res.status(500).json({message: "Internal Server error"});
-    }
+    res.json({ message: "Blog has been updated sucessfully." });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
+/**
+ * Deleting Post
+ */
 
-/*** Assignment ***/
-//edit
+export const deleteBlog = async(req, res, next) => {
+  try {
+    const blogId = req.params.blogId;
+    const deleteBlog = await Blog.findByIdAndDelete(blogId, req.body, {
+      new: true,
+    });
+    if (!deleteBlog) {
+      return res.status(404).json({ message: "No Blogs found." });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error." });
+  }
+}
 
-//delete
-
-//middleware
--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -1,11 +1,13 @@
 //importing modules
-import express from "express";
-import {getAllBlogs, addBlog} from "../controller/blog-controller.js";
-
+import express from 'express'
+import { getAllBlogs, addBlog, updateBlog, deleteBlog } from '../controller/blog-controller.js'
 
 const blogRouter = express.Router();
 
-blogRouter.get("/", getAllBlogs);
-blogRouter.post("/add",addBlog);
+blogRouter.get('/', getAllBlogs);
+blogRouter.post('/add', addBlog);
+blogRouter.put("/:blogId",updateBlog);
+blogRouter.delete("/:blogId", deleteBlog);
+
 
 export default blogRouter;
